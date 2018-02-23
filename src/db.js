baaad94e78f28671baf8bd2ccda2ceaa607da59d
olinastath <olina.stath@nyu.edu>
2018-02-23 18:49:04 -0500

    added mongo key

--- src/db.js
+++ src/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const URLSlugs = require('mongoose-url-slugs');
 const passportLocalMongoose = require('passport-local-mongoose');
+const db = process.env.MONGODB_URI || require('./config.js').mongoKey;
 
 // Everything references each other. Might have some spaghetti here... Hopefull won't be using this.
 
@@ -62,22 +63,4 @@ mongoose.model('Mood', Mood);
 mongoose.model('Playlist', Playlist);
 mongoose.model('User', User);
 
-// is the environment variable, NODE_ENV, set to PRODUCTION? 
-let dbconf;
-if (process.env.NODE_ENV === 'PRODUCTION') {
-	// if we're in PRODUCTION mode, then read the configration from a file
-	// use blocking file io to do this...
-	const fs = require('fs');
-	const path = require('path');
-	const fn = path.join(__dirname, 'config.json');
-	const data = fs.readFileSync(fn);
-	// our configuration file will be in json, so parse it and set the
-	// conenction string appropriately!
-	const conf = JSON.parse(data);
-	dbconf = conf.dbconf;
-} else {
-	// if we're not in PRODUCTION mode, then use
-	dbconf = 'mongodb://localhost/rc3009';
-}
-
-mongoose.connect(dbconf);
+mongoose.connect(db);
