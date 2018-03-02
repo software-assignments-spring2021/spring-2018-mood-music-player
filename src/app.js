82490d37e9ba68fe20d08e87a104275aa89f6b78
rchau0623 <rchau0623@gmail.com>
2018-03-01 22:49:14 -0500

    Adding some passport stuff

--- src/app.js
+++ src/app.js
@@ -47,11 +47,94 @@ passport.deserializeUser(User.deserializeUser());
 const clientId = process.env.CLIENTID;
 const clientSecret = process.env.CLIENTSECRET;
 
+// load routes from routes folder
+const placesRoutes = require('./routes/places');
+app.use('/api', placesRoutes);
+
 const db = process.env.MONGODB_URI || require('./config.js').mongoKey;
 mongoose.connect(db);
 
-app.get('/', (req, res) => {
+function authenticated(req,res,next) {
+	if(req.user) {
+		return next();
+	} else {
+		const message = 'you are not authenticated to view this page<br><a href="/">please log in &rarr;</a>';
+		res.render('error', {message: message});
+	}
+}
+
+// TODO: decide if any of this works with angular.js
+
+// TODO: check if this owrks.
+app.get('/', function(req, res) {
+	if (authenticated) {
+		// TODO: add .hbs of our app
+		res.render('login');
+	}
 	res.render('index');
+});
+
+// TODO: change error messages and render info
+app.post('/register', function(req, res, next) {
+	console.log('registering user');
+	// TODO: update findOne with appropriate information.
+	User.findOne({username: req.body.username}, function(err, user) {
+		if (err) {
+			return next(err);
+		} else if (user) {
+			return res.render('index', {message: 'user already exists'});
+		} else if (req.body.username === '' || req.body.password === '') {
+			return res.render('index', {message: 'enter username and password'});
+		} else {
+			// TODO: add user schema with user input here. 
+			const user = new User({
+				
+			});
+			User.register(user, req.body.password, function(err) {
+				if (err) {
+					return next(err);
+				}
+				passport.authenticate('local')(req, res, function () {
+					req.session.save(function (err) {
+					if (err) {
+						return next(err);
+					}
+					// TODO: redirect to our app. This could potentially be correct if the .get for '/' works.
+					res.redirect('/');
+					});
+				});
+				console.log('user registered!');
+			});
+		}
+	});
+});
+
+// TODO: change error messages and render info
+app.post('/login', function(req, res, next) {
+	passport.authenticate('local', function(err, user, info) {
+		if (err) { return next(err); }
+		if (!user) {
+			if (info.name === 'IncorrectUsernameError') {
+				return res.render('index', {message: 'username is incorrect' });
+			} else if (info.name === 'IncorrectPasswordError') {
+				return res.render('index', {message: 'password is incorrect' });
+			}
+		} 
+		req.logIn(user, function(err) {
+			if (!user) {
+				return res.render('index', {message: 'please enter username & password' });
+			} else {
+				if (err) { return next(err); }
+				// TODO: decide if we are using this. 
+				return res.redirect('/user/' + user.username);	
+			}
+		});
+	})(req, res, next);
+});
+
+app.get('/logout', function(req, res) {
+	req.logout();
+	res.redirect('/');
 });
 
 app.get('*', function(req, res) {
@@ -60,27 +143,3 @@ app.get('*', function(req, res) {
 
 // listening
 app.listen(process.env.PORT || 3000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
