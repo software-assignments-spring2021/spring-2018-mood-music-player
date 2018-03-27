057220a095ef8aac9a8ea7a4bc23fdf0c7b96368
olinastath <olina.stath@nyu.edu>
2018-03-27 03:10:30 -0400

    separated lyrics and sentiment into route

--- src/app.js
+++ src/app.js
@@ -20,66 +20,6 @@ const db = process.env.MONGODB_URI || require('./config.js').mongoKey;
 mongoose.connect(db);
 const app = express();
 
-
-// lyric-get api
-var lyric = require('lyric-get');
-var sentiment = require('sentiment');
-// todo: change the variables to equal the artist and title retrieved from spotify API. 
-var artist = 'Archive', title = 'Fuck U';
-
-// todo: get the artist, song name, from spotify api and place as parameters, make the parameters dynamic.
-// https://github.com/rhnvrm/lyric-api
-lyric.get(artist, title, function(err, res){
-    if (err) {
-        console.log(err);
-    }
-    else{
-		var songLyrics = res;
-
-		// sentiment API
-		// TODO: make code better, make sentiment code separate from lyrics-api code, get lyrics from lyric-api to sentiment.
-		var r1 = sentiment(songLyrics);
-		console.log(r1);        // Score: -2, Comparative: -0.666
-
-    }
-});
-
-
-
-// GraceNote API
-// TODO: fully integrate GraceNote
-
-// var Gracenote = require("node-gracenote");
-// var clientId2 = "526687528-85C47B0F083E1455C38E5A45FF411100";
-// var clientTag = "85C47B0F083E1455C38E5A45FF411100";
-// var userId = null;
-// var api = new Gracenote(clientId2, clientTag,userId);
-// api.register(function(err, uid) {
-// 	// TODO: store this somewhere for the next session
-// 	if (err) {
-// 		console.log(err);
-// 	} else {
-// 		console.log("You are good");
-// 	}
-// }); 
-
-var Gracenote = require("node-gracenote");
-var clientId2 = "269157806-FD674712D1DBE1B15D6CA2D43C847587";
-var clientTag = "FD674712D1DBE1B15D6CA2D43C847587";
-var userId = "49920687231833180-86F326FBA4C5F2EC7AB23B34DC1FF8C2";
-var api = new Gracenote(clientId2, clientTag, userId);
-api.searchTrack("NSYNC", "No Strings Attached", "It's Gonna Be Me", function(err, result) {
-	// Search Result as array
-	if (err) {
-		console.log(err);
-	} else {
-		console.log(result[1]);
-	}
-});
-
-
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
