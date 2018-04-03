f22d6db4c90fbf30d97295d5f42c980c6a453373
olinastath <olina.stath@nyu.edu>
2018-04-03 11:58:24 -0400

    fixed typo

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine,' 'ejs');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(session({
