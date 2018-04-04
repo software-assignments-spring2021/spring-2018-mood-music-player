4fba5750134a6824ea548f5f05732651f8680ece
olinastath <olina.stath@nyu.edu>
2018-04-04 01:26:24 -0400

    added functionality to refresh token button

--- src/public/controllers/main.js
+++ src/public/controllers/main.js
@@ -4,7 +4,12 @@
 
 	module.controller('MainController', function($scope, $http, $cookies){
     	$scope.refresh = function() {
-    		console.log('hi! ' + $cookies.refresh_token);
+    		console.log('inside refresh');
+    		$http.get('/spotify/refresh_token?refresh_token=' + $cookies.refresh_token).success(function(data) {
+				console.log("BEFORE:\n" + $cookies.token);
+				$cookies.token = data.access_token;
+				console.log("AFTER:\n" + $cookies.token);
+			});
     	};
 	});
     
