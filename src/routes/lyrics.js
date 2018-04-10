abb4f27951fc9aa56cda81072f268005289bd167
rchau0623 <rchau0623@gmail.com>
2018-04-10 00:52:14 -0400

    Working lyric-test

--- src/routes/lyrics.js
+++ src/routes/lyrics.js
@@ -8,13 +8,13 @@ router.get('/', function(req, res) {
 	// https://github.com/rhnvrm/lyric-api
 	var artist = req.query.artist;
 	var song = req.query.song;
-	lyric.get(artist, song, function(err, res) {
+	lyric.get(artist, song, function(err, response) {
 		if (err) {
 			console.log(err);
 		} else {
 			// sentiment API
-			const r1 = sentiment(res.lyric);
-			res.send({sentiment: r1, lyric: res.lyric});
+			const r1 = sentiment(response);
+			res.send({sentiment: r1, lyric: response});
 		}
 	});
 });
