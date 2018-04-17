8297271e53be8bebc3f7a4c50904006a7be89725
rchau0623 <rchau0623@gmail.com>
2018-04-17 13:10:11 -0400

    Adding readable format

--- src/public/services/mood.js
+++ src/public/services/mood.js
@@ -27,11 +27,36 @@
 				//let output2 = net2.run(song.analysis);
 				let energy = output.energy_level;
 				let valence = output.valence_level;
-				if ((energy > 0 && energy < .125) && (valence > 0 && valence < .125)) {
+				if ((energy >= 0 && energy <= .125) && (valence >= 0 && valence <= .125)) {
 					// Somber
-				} else if ((energy > 0 && energy < .125) && (valence > 0 && valence < .125)) {
-
+				} else if ((energy >= 0 && energy <= .125) && (valence > .125 && valence <= .375)) {
+					// Ominous
+				} else if ((energy >= 0 && energy <= .125) && (valence > .375 && valence <= .625)) {
+					// Sentimental
+				} else if ((energy >= 0 && energy <= .125) && (valence > .625 && valence <= .875)) {
+					// Nostalgic
+				} else if ((energy >= 0 && energy <= .125) && (valence > .625 && valence <= 1)) {
+					// Peaceful
+				} else if ((energy > .125 && energy <= .375) && (valence >= 0 && valence <= .125)) {
+					// Depressing
+				} else if ((energy > .125 && energy <= .375) && (valence > .125 && valence <= .375)) {
+					// Melancholy
+				} else if ((energy > .125 && energy <= .375) && (valence > .375 && valence <= .625)) {
+					// Mellow
+				} else if ((energy > .125 && energy <= .375) && (valence > .625 && valence <= .875)) {
+					// Tender
+				} else if ((energy > .125 && energy <= .375) && (valence > .875 && valence <= 1)) {
+					// Easygoing
+				} else if ((energy > .375 && energy <= .625) && (valence > .625 && valence <= .875)) {
+					// Optimistic
+				} else if ((energy > .375 && energy <= .625) && (valence > .875 && valence <= 1)) {
+					// Content
 				}
+
+
+
+
+
 
 			}
 
