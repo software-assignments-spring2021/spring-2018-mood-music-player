2e283df434e5901ef4fcf995a0415f0f44a3e0f6
olinastath <olina.stath@nyu.edu>
2018-04-19 13:37:49 -0400

    added update button, still need to test

--- src/public/controllers/main.js
+++ src/public/controllers/main.js
@@ -2,7 +2,7 @@
 
 	var module = angular.module('smoodifyApp');
 
-	module.controller('MainController', function($scope, PlayerAPI, SpotifyAPI, $http, $cookies, $rootScope, $location){
+	module.controller('MainController', function($scope, $http, $cookies, $rootScope, $location, $window, PlayerAPI, SpotifyAPI, DatabaseService){
 		$scope.refresh = function() {
 			SpotifyAPI.refreshToken().then(function(token) {
 				console.log('BEFORE:', $cookies.token);
@@ -11,15 +11,6 @@
 			});
 		};
 		
-		// Error handling
-		// $scope.player.addListener('initialization_error', ({ message }) => { console.error(message); });
-		// $scope.player.addListener('authentication_error', ({ message }) => { console.error(message); });
-		// $scope.player.addListener('account_error', ({ message }) => { console.error(message); });
-		// $scope.player.addListener('playback_error', ({ message }) => { console.error(message); });
-
-		// Playback status updates
-		// $scope.player.addListener('player_state_changed', state => { console.log(state.shuffle); });
-
 		/* Play a song. Trigger this function when play button is pressed */
 		$scope.play = function() {
 			PlayerAPI.getPlayerState().then(function(data) {
@@ -96,6 +87,44 @@
 		$scope.playSong = function(song_uri) {
 			PlayerAPI.playClickedSong();
 		};
+
+		$scope.updateSongs = function() {
+			SpotifyAPI.getTracks().then(function(allTracks) {
+				const current = $rootScope.current_user.saved_songs.length;
+				if (allTracks.length > current) {
+					for (var i = 0; i < allTracks.length - current; i++) {
+						console.log("inside allTracks");
+						var artists = allTracks[i].artists.map(function(a) {
+							return {
+								name: a.name,
+								spotify_id: a.id,
+								spotify_uri: a.uri
+							}
+						});		// artists array
+						var album = {
+							name: allTracks[i].album.name,
+							images: allTracks[i].album.images,
+							spotify_id: allTracks[i].album.id,
+							spotify_uri: allTracks[i].album.uri
+						} // album object
+						var song = {
+							name: allTracks[i].name,
+							artist: artists,
+							album: album,
+							id: allTracks[i].id,
+							uri: allTracks[i].uri,
+							duration_ms: allTracks[i].duration_ms
+						}
+						DatabaseService.saveSongToUser($rootScope.current_user.username, song).then(function(d) {
+							$window.localStorage.setItem('user', JSON.stringify(d.data));
+						});
+					};
+				} else {
+					console.log("up to date");
+					// add popup saying "everything is up to date"
+				}
+			});
+		}
 	});
     
 })();
