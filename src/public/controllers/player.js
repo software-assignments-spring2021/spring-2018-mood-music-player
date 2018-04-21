d4665f56ea10caa65e1e983888b844c951a55d93
olinastath <olina.stath@nyu.edu>
2018-04-21 18:10:33 -0400

    fixed update

--- src/public/controllers/player.js
+++ src/public/controllers/player.js
@@ -2,7 +2,7 @@
 
 	var module = angular.module('smoodifyApp');
 
-	module.controller('PlayerController', function($scope, $http, $cookies, $rootScope, $interval, $window, PlayerAPI, SpotifyAPI, MoodService, DatabaseService) {
+	module.controller('PlayerController', function($scope, $http, $cookies, $rootScope, $location, $interval, $window, $route, PlayerAPI, SpotifyAPI, MoodService, DatabaseService) {
 		/* created spotify web sdk playback code into a ng-click function called by clicking a temp button in main.html */
 		if ($rootScope.player === undefined) {
 			PlayerAPI.initialize().then(function(player) {
@@ -15,7 +15,6 @@
 		var width = 0;
 		var progress_ms = 0;
 		var duration_ms = 0;
-
 
 		/* Make the progress bar progress */
 		$interval(function() {
@@ -216,30 +215,30 @@
 					for (var i = 0; i < allTracks.length - current; i++) {
 						console.log(allTracks[i]);
 						console.log("inside allTracks");
-						// var artists = allTracks[i].artists.map(function(a) {
-						// 	return {
-						// 		name: a.name,
-						// 		spotify_id: a.id,
-						// 		spotify_uri: a.uri
-						// 	}
-						// });		// artists array
-						// var album = {
-						// 	name: allTracks[i].album.name,
-						// 	images: allTracks[i].album.images,
-						// 	spotify_id: allTracks[i].album.id,
-						// 	spotify_uri: allTracks[i].album.uri
-						// } // album object
-						// var song = {
-						// 	name: allTracks[i].name,
-						// 	artist: artists,
-						// 	album: album,
-						// 	id: allTracks[i].id,
-						// 	uri: allTracks[i].uri,
-						// 	duration_ms: allTracks[i].duration_ms
-						// }
-						// DatabaseService.saveSongToUser($rootScope.current_user.username, song).then(function(d) {
-						// 	$window.localStorage.setItem('user', JSON.stringify(d.data));
-						// });
+						var artists = allTracks[i].artists.map(function(a) {
+							return {
+								name: a.name,
+								spotify_id: a.id,
+								spotify_uri: a.uri
+							}
+						});		// artists array
+						var album = {
+							name: allTracks[i].album.name,
+							images: allTracks[i].album.images,
+							spotify_id: allTracks[i].album.id,
+							spotify_uri: allTracks[i].album.uri
+						} // album object
+						var song = {
+							name: allTracks[i].name,
+							artist: artists,
+							album: album,
+							id: allTracks[i].id,
+							uri: allTracks[i].uri,
+							duration_ms: allTracks[i].duration_ms
+						}
+						DatabaseService.saveSongToUser($rootScope.current_user.username, song).then(function(d) {
+							$window.localStorage.setItem('user', JSON.stringify(d.data));
+						});
 					};
 				} else {
 					console.log("up to date");
